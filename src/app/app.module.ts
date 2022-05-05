bd84bc4b55ed299283b2fb13c10808592b0c05a7
Jade-abr <jade@finalgo.fr>
2022-05-05 16:22:40 +0200

    first step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FaceSnapComponent} from "./face-snap/face-snap.component";
+import {FaceSnapListComponent} from "./face-snap-list/face-snap-list.component";
+import {HeaderComponent} from "./header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceSnapComponent,
+    FaceSnapListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
