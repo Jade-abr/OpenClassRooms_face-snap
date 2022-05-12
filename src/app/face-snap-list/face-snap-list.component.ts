4340847ad44d5efe7f1c8725da927eed227a140c
Jade-abr <jade@finalgo.fr>
2022-05-12 17:02:05 +0200

    ngOnDestroy

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FaceSnap} from "../models/face-snap.model";
 import {FaceSnapsService} from "../services/face-snaps.service";
-import {interval, take, tap} from "rxjs";
+import {interval, Subject, takeUntil, tap} from "rxjs";
 
 
 @Component({
@@ -9,18 +9,26 @@ import {interval, take, tap} from "rxjs";
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>; // $ à la fin pour dire que c'est un Observable
 
   constructor(private faceSnapsService: FaceSnapsService) { }
 
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsService.faceSnaps;
+    this.destroy$ = new Subject<boolean>();
+
     interval(1000).pipe(
-      take(3),
-      tap(console.log)
+      tap(console.log),
+      takeUntil(this.destroy$)
     ).subscribe();
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 
 }
