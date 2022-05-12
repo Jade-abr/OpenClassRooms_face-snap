7877f373dae4e0ba3aa5ff3f5936b40b72908e8d
Jade-abr <jade@finalgo.fr>
2022-05-12 15:57:39 +0200

    opérateurs bas niveaux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {filter, interval, map, Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'facesnap';
+export class AppComponent implements OnInit {
+  interval$!: Observable<string>;
+
+  ngOnInit() {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0), // uniquement les valeurs divibles par 3
+      map(value => value % 2 === 0 ? // si la valeur est divible par 2 alors c'est pair sinon impair
+        `Je suis ${value} et je suis pair` :
+        `Je suis ${value} et je suis impair`
+      ),
+      tap(text => this.logger(text))
+    );
+  }
+
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
+  }
 }
+
+
