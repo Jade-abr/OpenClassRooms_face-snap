65f3ea2717ee920a0be768e01dcfae028bf3af1d
Jade-abr <jade@finalgo.fr>
2022-05-12 16:06:53 +0200

    opérateurs haut niveaux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {filter, interval, map, Observable, tap} from "rxjs";
+import {delay, filter, interval, map, mergeMap, Observable, of, take, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,21 +7,31 @@ import {filter, interval, map, Observable, tap} from "rxjs";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  interval$!: Observable<string>;
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 
   ngOnInit() {
-    this.interval$ = interval(1000).pipe(
-      filter(value => value % 3 === 0), // uniquement les valeurs divibles par 3
-      map(value => value % 2 === 0 ? // si la valeur est divible par 2 alors c'est pair sinon impair
-        `Je suis ${value} et je suis pair` :
-        `Je suis ${value} et je suis impair`
-      ),
-      tap(text => this.logger(text))
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      mergeMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+  }
+
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
     );
   }
 
-  logger(text: string): void {
-    console.log(`Log: ${text}`);
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
   }
 }
 
