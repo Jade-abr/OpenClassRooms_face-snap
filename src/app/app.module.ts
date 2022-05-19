8ffe76a62bd511e6b6e76566cf9347ac29bba022
Jade-abr <jade@finalgo.fr>
2022-05-19 16:39:38 +0200

    code module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
-import {LOCALE_ID, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FaceSnapComponent} from "./face-snap/face-snap.component";
 import {FaceSnapListComponent} from "./face-snap-list/face-snap-list.component";
-import {HeaderComponent} from "./header/header.component";
-import {registerLocaleData} from "@angular/common";
-import * as fr from '@angular/common/locales/fr';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
 import {HttpClientModule} from "@angular/common/http";
-import {httpInterceptorProviders} from "./interceptor";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapComponent,
     FaceSnapListComponent,
-    HeaderComponent,
     LandingPageComponent,
     SingleFaceSnapComponent,
     NewFaceSnapComponent
@@ -30,16 +26,14 @@ import {httpInterceptorProviders} from "./interceptor";
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [
-    { provide: LOCALE_ID, useValue: 'fr-FR' },
-    httpInterceptorProviders
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
   constructor() {
-    registerLocaleData(fr.default);
   }
 }
